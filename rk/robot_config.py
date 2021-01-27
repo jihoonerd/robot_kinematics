430d228243a945253f15d486a402d80a2dfc7013
Jihoon Kim <jihoonerd@outlook.com>
2021-01-28 06:40:02 +0900

    Fix indexing in fk

--- rk/robot_config.py
+++ rk/robot_config.py
@@ -18,20 +18,8 @@ class LinkNode:
 
 class RobotObject:
 
-    def __init__(self):
-        
-        self.ulink = {
-            0: LinkNode(name="NULL", id=0), # 0 is reserved for NULL
-            1: LinkNode(name='BODY', id=1, child=2, sister=0, mother=0),
-            2: LinkNode(name='RARM', id=2, child=3, sister=4, mother=1),
-            3: LinkNode(name='RHAND', id=3, child=0, sister=0, mother=2),
-            4: LinkNode(name='LARM', id=4, child=5, sister=6, mother=1),
-            5: LinkNode(name='LHAND', id=5, child=0, sister=0, mother=4),
-            6: LinkNode(name='RLEG', id=6, child=7, sister=8, mother=1),
-            7: LinkNode(name='RFOOT', id=7, child=0, sister=0, mother=6),
-            8: LinkNode(name='LLEG', id=8, child=9, sister=0, mother=1),
-            9: LinkNode(name='LFOOT', id=9, child=0, sister=0, mother=8),
-        }
+    def __init__(self, ulink):
+        self.ulink = ulink
 
     def print_link_name(self, link_id):
         
@@ -49,11 +37,11 @@ class RobotObject:
             return None
         if node_id != 1: # If node is not body
             mother = self.ulink[node_id].mother
-            self.ulink[node_id].p = self.ulink[mother.id].R @ self.ulink[node_id].b + self.ulink[mother.id].p
-            self.ulink[node_id].R = self.ulink[mother.id].R @ self.rodrigues(self.ulink[node_id].a, self.ulink[node_id].q)
+            self.ulink[node_id].p = self.ulink[mother].R @ self.ulink[node_id].b + self.ulink[mother].p
+            self.ulink[node_id].R = self.ulink[mother].R @ self.rodrigues(self.ulink[node_id].a, self.ulink[node_id].q)
 
-        self.forward_kinematics(self.ulink[node_id].sister.id)
-        self.forward_kinematics(self.ulink[node_id].child.id)
+        self.forward_kinematics(self.ulink[node_id].sister)
+        self.forward_kinematics(self.ulink[node_id].child)
 
     def rodrigues(self, w, dt):
         """This returns SO(3) from so(3)
@@ -70,8 +58,8 @@ class RobotObject:
             R = np.eye(3)
         else:
             wn = w/norm_w # rotation axis (unit vector)
-            th = norm_w @ dt # amount of rotation (rad)
-            w_wedge = np.array([[0, -wn[3], wn[2]], [wn[3], 0, -wn[1]], [-wn[2], wn[1], 0]])
+            th = norm_w * dt # amount of rotation (rad)
+            w_wedge = np.array([[0, -wn[2], wn[1]], [wn[2], 0, -wn[0]], [-wn[1], wn[0], 0]])
             R = np.eye(3) + w_wedge * np.sin(th) + np.linalg.matrix_power(w_wedge, 2) * (1-np.cos(th))
         return R
     
@@ -126,12 +114,3 @@ class RobotObject:
         else:
             w = np.math.pi/2 * np.array([[R[0,0]+1], [R[1,1]+1], [R[2,2]+1]])
         return w
-            
-if __name__ == "__main__":
-    print()
-    ro = RobotObject()
-    for k, v in ro.ulink.items():
-        print(f"{k} : {v.name} {v.id} {v.sister} {v.child} ")
-
-    print(ro.ulink[ro.ulink[2].child].name)
-    print(ro.ulink[ro.ulink[ro.ulink[2].id].sister].name)
