ad28ee7463593a3e2cbabb239958fb82e6557023
Jihoon Kim <jihoonerd@outlook.com>
2021-02-04 11:27:07 +0900

    Visualize links

--- src/viz/visualizer.py
+++ src/viz/visualizer.py
@@ -29,8 +29,8 @@ def visualizer():
 
         if ulink:
             data = pickle.loads(ulink)
+            joint_list = []
             for joints in data.values():
-
                 marker = Marker(
                     header=Header(frame_id='my_frame', stamp=rospy.Time.now()),
                     id=joints.id,
@@ -41,6 +41,7 @@ def visualizer():
                     color=ColorRGBA(1, 0, 0, 1),
                     lifetime=rospy.Duration()
                 )
+                joint_list.append(marker)
                 marker_array.markers.append(marker)
 
                 # Add gizmos
@@ -80,6 +81,19 @@ def visualizer():
                 marker_array.markers.append(y_gizmo)
                 marker_array.markers.append(z_gizmo)
             
+            # link
+            line_list = Marker(
+                            header=Header(frame_id='my_frame', stamp=rospy.Time.now()),
+                            action=Marker.ADD,
+                            id=41,
+                            type=Marker.LINE_STRIP,
+                            color=ColorRGBA(1.0, 1.0, 1.0, 1.0)
+                        )
+            line_list.scale.x = 0.02
+            line_list.pose.orientation.w = 1.0
+            for marker in joint_list:
+                line_list.points.append(marker.pose.position)
+            marker_array.markers.append(line_list)
             pub.publish(marker_array)
         rate.sleep()
     
